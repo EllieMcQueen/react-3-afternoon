9f159d73abb94266f44a37b9056f7c9977b2b9da
Ellie McQueen <elliemcqueen@Ellies-MacBook-Pro.local>
2020-09-24 19:29:19 -0600

    update

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -50,54 +50,67 @@ export default class Post extends Component {
   }
 
   render() {
-      const { editing, showMasterMenu } = this.state;
-      const { text, date } = this.props;
-    
-      return (
-        <section className="Post__parent" onClick={ this.hideMasterMenu }>
-    
-          <div className="Post__master-controls">
-            <MasterControlIcon onClick={ this.toggleMasterMenu } />
-    
-            <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
-              <span onClick={ this.showEdit }>Edit</span>
-              <span>Delete</span>
-            </div>
-          </div>
-    
-          <div className="Post__meta-data">
-            <div className="Post__profile-picture">
-              <ProfileIcon />
-            </div>
-    
-            <span className="Post__name">DevMountain</span>
-            <span className="Post__handle">@DevMountain</span>
-    
-            <span className="Post__date">- { date }</span>
-          </div>
-    
-          <div className="Post__content">
-            {
-              editing
-              ?
-             <Edit text={ text }
-                    id={ id } // Remember to destructure id off of props or use this.props.id
-                    hideEdit={ this.hideEdit }
-                    updatePostFn={ updatePostFn } />
-    :
-      <span className="Post__text">{ text }</span>
-  }
-      </div>
-    
-          <div className="Post__user-controls">
-            <ReplyIcon className="Post__control-icon" />
-            <FavoriteIcon className="Post__control-icon" />
-            <MessageIcon className="Post__control-icon" />
-          </div>
-    
-        </section>
-      );
-}
-  }
+    const { editing, showMasterMenu } = this.state;
+    const { text, date, id, updatePostFn, deletePostFn } = this.props;
+  
+    return (
+      <section className="Post__parent" onClick={ this.hideMasterMenu }>
+  
+         {/* Three dots in top right corner */}
+        <div className="Post__master-controls">
+          <MasterControlIcon onClick={ this.toggleMasterMenu } />
 
-    
+          {/* Drop-down menu. Remember that the "showMasterMenu" variable has been destructured off of this.state */}
+        <div className="Post__master-menu" style={ { display: showMasterMenu ? 'flex' : 'none' } }>
+        <span onClick={ this.showEdit }>Edit</span>
+        <span onClick={ () => deletePostFn( id ) }>Delete</span> { /* Remember to destructors deletePostFn off of props or use this.props.deletePostFn */ }
+        </div>
+        </div>
+        
+         {/* This is where all the meta data of the post will go (who, when, where) */}
+        <div className="Post__meta-data">
+          <div className="Post__profile-picture">
+            <ProfileIcon />
+        </div>
+  
+          <span className="Post__name">DevMountain</span>
+          <span className="Post__handle">@DevMountain</span>
+  
+          <span className="Post__date">- { date }</span>
+        </div>
+
+        {/* This is where the text goes. Notice the turnary statement. The turnary statement decides to display either the text OR the editor view
+            You can also think of it as being written as so:
+              if( this.state.editing === true ) {
+                <Edit ... />
+              } else {
+                <span ... ></span>
+              }
+        */}
+  
+        <div className="Post__content">
+          {
+            editing
+            ?
+            <Edit text={ text }
+                id={ id } 
+                hideEdit={ this.hideEdit }
+                updatePostFn={ updatePostFn } /> 
+            :
+            <span className="Post__text">{ text }</span>
+          }
+        </div>
+  
+
+        {/* These are all of the cute little icons in the bottom left corner */}
+
+        <div className="Post__user-controls" >
+          <ReplyIcon className="Post__control-icon" />
+          <FavoriteIcon className="Post__control-icon" />
+          <MessageIcon className="Post__control-icon" />
+        </div>
+  
+      </section>
+    )
+  }
+}
